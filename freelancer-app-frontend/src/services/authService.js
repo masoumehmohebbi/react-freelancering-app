7782e0ed2a770428f1b3f09374ab783665df585b
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-11-27 23:01:36 +0330

    Added completeProfile

--- freelancer-app-frontend/src/services/authService.js
+++ freelancer-app-frontend/src/services/authService.js
@@ -8,3 +8,8 @@ export function getOtp(data) {
 export function checkOtp(data) {
   return http.post("/user/get-otp", data).then(({ data }) => data.data);
 }
+export function completeProfile(data) {
+  return http
+    .post("/user/complete-profile", data)
+    .then(({ data }) => data.data);
+}
