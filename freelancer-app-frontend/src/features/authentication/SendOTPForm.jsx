d4402ebda780aa98da91336d494f7824fedcc2eb
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-11-27 09:07:23 +0330

    Added loading: SendOTPForm.jsx

--- freelancer-app-frontend/src/features/authentication/SendOTPForm.jsx
+++ freelancer-app-frontend/src/features/authentication/SendOTPForm.jsx
@@ -1,13 +1,31 @@
 import TextField from "../../ui/TextField";
+import Loading from "../../ui/Loading";
 
-const SendOTPForm = () => {
+const SendOTPForm = ({
+  isSendingOtp,
+  onSubmit,
+  setStep,
+  phoneNumber,
+  onChange,
+}) => {
   return (
     <div>
-      <form className="space-y-8">
-        <TextField label="شماره موبایل" name="phoneNumber" />
-        <button type="submit" className="btn btn--primary w-full">
-          ارسال کد تایید
-        </button>
+      <form className="space-y-8" onSubmit={onSubmit}>
+        <TextField
+          label="شماره موبایل"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={onChange}
+        />
+        <div>
+          {isSendingOtp ? (
+            <Loading />
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              ارسال کد تایید
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
