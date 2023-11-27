9f4a1b09c2b921c5dd60dc99c3389af20c418e5e
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-11-27 23:01:17 +0330

    Added CompleteProfile: App.jsx

--- freelancer-app-frontend/src/App.jsx
+++ freelancer-app-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import CompleteProfile from "./pages/CompleteProfile";
 function App() {
   const querClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
       <div className="container xl:max-w-screen-xl">
         <Routes>
           <Route path="/auth" element={<Auth />} />
+          <Route path="/complete-profile" element={<CompleteProfile />} />
         </Routes>
       </div>
     </QueryClientProvider>
