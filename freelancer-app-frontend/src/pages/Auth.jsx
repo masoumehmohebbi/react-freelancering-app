6da96b7254547ee90f026d18ba7c5c856587edd6
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-11-27 09:07:49 +0330

    Rendered AuthContainer: AuthContainer.jsx

--- freelancer-app-frontend/src/pages/Auth.jsx
+++ freelancer-app-frontend/src/pages/Auth.jsx
@@ -1,13 +1,9 @@
-import CheckOTPForm from "../features/authentication/CheckOTPForm";
-import SendOTPForm from "../features/authentication/SendOTPForm";
+import AuthContainer from "../features/authentication/AuthContainer";
 
 const Auth = () => {
   return (
     <div className="flex justify-center">
-      <div className="w-full sm:max-w-sm">
-        <SendOTPForm />
-        <CheckOTPForm />
-      </div>
+      <AuthContainer />
     </div>
   );
 };
