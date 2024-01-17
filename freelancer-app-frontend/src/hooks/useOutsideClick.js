6a93c6f5e97d16d83466e737555d6696e9a70a05
masoumeMohebbi <masoumeh.mohebbi.com>
2024-01-17 22:53:31 +0330

    💄 Added useOutsideClick

---
+++ freelancer-app-frontend/src/hooks/useOutsideClick.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+
+const useOutsideClick = ({ handler, listenCapturing = true }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        handler();
+      }
+    }
+
+    document.addEventListener("click", handleClick, listenCapturing);
+    return () => document.removeEventListener(handleClick, listenCapturing);
+  }, [handler, listenCapturing]);
+  return ref;
+};
+
+export default useOutsideClick;
