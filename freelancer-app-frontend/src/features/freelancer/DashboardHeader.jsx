74343ecd8662169320c4aa170afbd8b371d488ec
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-01-25 12:36:56 +0330

    ✅ Added proposalTable

--- freelancer-app-frontend/src/features/freelancer/DashboardHeader.jsx
+++
@@ -1,5 +0,0 @@
-const DashboardHeader = () => {
-  return <div>DashboardHeader</div>;
-};
-
-export default DashboardHeader;
--- freelancer-app-frontend/src/pages/Proposals.jsx
+++ freelancer-app-frontend/src/pages/Proposals.jsx
@@ -1,5 +1,14 @@
+import ProposalTable from "../features/proposals/ProposalTable";
+
 const Proposals = () => {
-  return <div>Proposals</div>;
+  return (
+    <div>
+      <h1 className="font-black text-secondary-700 text-xl mb-8">
+        پروپوزال های شما
+      </h1>
+      <ProposalTable />
+    </div>
+  );
 };
 
 export default Proposals;
