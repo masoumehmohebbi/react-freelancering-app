cb145a2e93148bb9e76af595f26cca48463d39b5
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-01-27 11:24:27 +0330

    📦 Query string PACKAGE for Filtering

--- freelancer-app-frontend/src/hooks/useProjects.js
+++ freelancer-app-frontend/src/hooks/useProjects.js
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProjectsApi } from "../services/projectService";
+import { useLocation } from "react-router-dom";
+import { queryString } from "query-string";
 
 export default function useProjects() {
+  const { search } = useLocation();
+  const queryObject = queryString.parse(search);
   const { data, isLoading } = useQuery({
-    queryKey: ["projects"],
-    queryFn: getProjectsApi,
+    queryKey: ["projects", queryObject],
+    queryFn: () => getProjectsApi(search),
     retry: false,
   });
 
