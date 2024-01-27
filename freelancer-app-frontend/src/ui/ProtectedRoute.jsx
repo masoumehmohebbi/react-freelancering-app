89979fffb5ca9c3f069047615b4af8ab8222ee24
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-01-27 12:57:00 +0330

    ✅ Protected Route

--- freelancer-app-frontend/src/ui/ProtectedRoute.jsx
+++ freelancer-app-frontend/src/ui/ProtectedRoute.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+import useAuthorized from "../features/authentication/useAuthorized";
+import Loading from "./Loading";
+import { useNavigate } from "react-router-dom";
+
 const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticate, isAuthorized } = useAuthorized();
+
+  useEffect(() => {
+    if (!isAuthenticate && !Loading) navigate("/auth");
+    if (!isAuthorized && !Loading) navigate("/not-access", { replace: true });
+  }, [isAuthorized, navigate, isAuthenticate, isLoading]);
+
+  if (Loading)
+    return (
+      <div className="flex items-center h-screen justify-center bg-secondary-100">
+        <Loading />
+      </div>
+    );
   return children;
 };
 
